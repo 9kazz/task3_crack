c0db7042e3619f14e58339bf5e8481c40beebedc
9kazz <gkazunin@gmail.com>
2026-03-11 22:40:29 +0300

    2v: working vers

--- hard.asm
+++ hard.asm
@@ -8,10 +8,26 @@ org 100h
 ;==================================================================================================================
 
 Start:              call Main
-                    mov ax, 4c00h
+
+                    push 0
+                    pop es
+
+                    cli
+                    mov word ptr es:[72h * 4], offset Main
+                    mov ax, cs
+                    mov word ptr es:[72h * 4 + 2], ax
+                    sti
+
+                    mov ax, 3100h                 
+                    mov dx, offset End_of_resident
+                    shr dx, 4                     
+                    inc dx                        
                     int 21h
 
 Main                proc
+                    
+                    mov ax, cs
+                    mov ds, ax
 
                     mov ah, 09h
                     mov dx, offset Text_1
@@ -19,14 +35,6 @@ Main                proc
 
                     mov ah, 0ah                     ; password input into the Password_buf
                     mov dx, offset Password_buf
-                    int 21h
-
-                    mov ah, 09h
-                    mov dx, offset Text_2
-                    int 21h
-
-                    mov ah, 0ah                     ; input user review
-                    mov dx, offset User_review
                     int 21h
 
                     mov si, offset Cor_Password
@@ -40,13 +48,21 @@ Main                proc
 @@access_granted:   mov ah, 09h
                     mov dx, offset Success_ans
                     int 21h
-                    ret
+                    jmp @@user_review
 
 @@access_denied:    mov ah, 09h
                     mov dx, offset Failure_ans
                     int 21h
-                    ret
 
+@@user_review:      mov ah, 09h
+                    mov dx, offset Text_2
+                    int 21h
+
+                    mov ah, 0ah                     ; input user review
+                    mov dx, offset User_review
+                    int 21h
+
+                    ret
                     endp
 
 ;------------------------------------------------------------------------------------------------------------------
@@ -58,9 +74,11 @@ User_review         db 10, 12 dup(0)
 Failure_ans         db 0dh, 0ah, "Access denied!$", 1 dup(?)
 Success_ans         db 0dh, 0ah, "Access granted!$"
 
-Text_1              db "Enter password:$"
-Text_2              db "Do you like this program? Your opinion is veeeery important for us!$"
+Text_1              db 0dh, 0ah, "Enter password:$"
+Text_2              db 0dh, 0ah, "Do you like this program? Your opinion is veeeery important for us!$"
 
 ;------------------------------------------------------------------------------------------------------------------
+
+End_of_resident:
 
 end                 Start
